6f25754433007376d60b005d941d5b587858f45f
MiguelChaves-pk <miguelchaves42@gmail.com>
2021-05-21 15:13:31 -0300

    i've finished a login screen fully responsive

---
+++ js/vv.js
@@ -0,0 +1,16 @@
+function abrirModal(){
+    document.getElementById('modal').style.top = "0";
+}
+
+function fecharModal(){
+    document.getElementById('modal').style.top = "-100%";
+
+}
+
+function loginFeito(){
+    window.location.href = "fb.com"
+}
+
+
+
+
